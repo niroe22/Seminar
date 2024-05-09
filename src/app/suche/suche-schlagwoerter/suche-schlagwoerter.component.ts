ca82561409c5e4bb61d841427640abfb83a7f3de
nkls <niki.roemer@gmail.com>
2024-05-09 13:36:36 +0200

    searchkeyword component working

--- src/app/suche/suche-schlagwoerter/suche-schlagwoerter.component.ts
+++ src/app/suche/suche-schlagwoerter/suche-schlagwoerter.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Subject } from 'rxjs';
+import log from 'loglevel';
 
+/**
+ * Komponente f&uuml;r den CSS-Selektor <code>app-suche-schlagwoerter</code>
+ */
 @Component({
   selector: 'app-suche-schlagwoerter',
-  standalone: true,
-  imports: [],
   templateUrl: './suche-schlagwoerter.component.html',
-  styleUrl: './suche-schlagwoerter.component.css'
+  imports: [FormsModule],
+  standalone: true,
 })
 export class SucheSchlagwoerterComponent {
+  protected javascript = false;
 
+  protected typescript = false;
+
+  @Output()
+  protected readonly javascript$ = new Subject<boolean>();
+
+  @Output()
+  protected readonly typescript$ = new Subject<boolean>();
+
+  constructor() {
+    log.debug('SucheSchlagwoerterComponent()');
+  }
+
+  protected onChangeJavascript(event: Event) {
+    // https://stackoverflow.com/questions/44321326/property-value-does-not-exist-on-type-eventtarget-in-typescript
+    const { checked } = event.target as HTMLInputElement;
+    log.debug(
+      `SucheSchlagwoerterComponent.onChangeJavascript: checked=${checked}`,
+    );
+    this.javascript$.next(checked);
+  }
+
+  protected onChangeTypescript(event: Event) {
+    const { checked } = event.target as HTMLInputElement;
+    log.debug(
+      `SucheSchlagwoerterComponent.onChangeTypescript: checked=${checked}`,
+    );
+    this.typescript$.next(checked);
+  }
 }
+
